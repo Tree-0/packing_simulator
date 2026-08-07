9be4699148bd141b6b530bf5f97e947a9184797c
Nathaniel Stall <117487029+Tree-0@users.noreply.github.com>
2026-08-06 23:48:44 -0500

    bottom left placement policy

--- backend/policy.go
+++ backend/policy.go
@@ -1,1 +1,38 @@
-package policy
+/*
+Policies to determine how objects are placed into the packing simulation
+*/
+
+package backend
+
+type PlacementDecision struct {
+	X int
+	Y int
+}
+
+type Policy interface {
+	FindPlacement(*Container, Box) (PlacementDecision, bool)
+	Name() string
+}
+
+// BottomLeftPolicy places boxes at the lowest, then leftmost, available position.
+type BottomLeftPolicy struct{}
+
+func (BottomLeftPolicy) Name() string {
+	return "bottom-left"
+}
+
+func (BottomLeftPolicy) FindPlacement(container *Container, box Box) (PlacementDecision, bool) {
+	if container == nil {
+		return PlacementDecision{}, false
+	}
+
+	for y := container.Height() - box.Height; y >= 0; y-- {
+		for x := 0; x <= container.Width()-box.Width; x++ {
+			if container.CanPlace(box, x, y) {
+				return PlacementDecision{X: x, Y: y}, true
+			}
+		}
+	}
+
+	return PlacementDecision{}, false
+}
